e7e52132cb411f651a487512857a3698c1b5b51a
Provi Zhang <zhangxian1942@live.com>
2016-03-10 04:30:47 +0000

    Fill in Cases Controller CRUD methods

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,35 +1,47 @@
 class CasesController < ApplicationController
-    
-    def case_params
-        params.require(:case, :name, :diagnosis).permit(:age, :gender, :disease_characteristics, :treatment_history, :past_medical_history)
+  def index
+    @cases = Case.all
+  end
+  
+  def new
+    @case = Case.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @case = Case.new(case_params)
+    if @case.save
+      flash[:success] = "Case was succesfully created"
+      redirect_to case_path(@case)
+    else
+      render :new
     end
-    
-    def show
-        
+  end
+  
+  def update
+    if @case.update(case_params)
+      flash[:success] = "Case was successfully updated"
+      redirect_to case_path(@case)
+    else
+      render :edit
     end
-    
-    def index
-        
-    end
-    
-    def new
-        
-    end
-    
-    def create
-        
-    end
-    
-    def edit
-        
-    end
-    
-    def update
-        
-    end
-    
-    def destroy
-        
-    end
-    
+  end
+  
+  def show
+  end
+  
+  def destroy
+    @case.destroy
+    flash[:danger] = "Case was successfully deleted"
+    redirect_to cases_path
+  end
+  
+  private
+  def case_params    
+    params.require(:case, :name, :diagnosis).permit(:age, :gender, 
+      :disease_characteristic, :treatment_history, :past_medical_history)
+  end
+
 end
