68901d1c592216d8ad63787d44e69533e8fb0c60
abraarakhter <akhterabraar@gmail.com>
2016-03-10 04:01:17 +0000

    added cases controller methods

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,2 +1,35 @@
 class CasesController < ApplicationController
+    
+    def case_params
+        params.require(:case).permit(:name, :diagnosis)
+    end
+    
+    def show
+        
+    end
+    
+    def index
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+    
 end
