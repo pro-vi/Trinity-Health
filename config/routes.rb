c1bb7eb8a6c01a8c94ceaf6d89f86c941316da58
Kamran Siddiqui <kamranfsiddiqui@berkeley.edu>
2016-03-09 20:09:19 -0800

    added resourceful routes for case model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  resources :cases
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
