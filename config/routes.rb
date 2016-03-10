ab6859be7510181341aef9d721b43bdc2eecc96d
Provi Zhang <zhangxian1942@live.com>
2016-03-10 06:22:00 +0000

    Quick fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   resources :cases
-  root :to => redirect('/cases/index')
+  root :to => redirect('/cases/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
