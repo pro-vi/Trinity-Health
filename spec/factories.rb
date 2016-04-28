b45bc8367245ae78d1d666b5c1851f2f29cab19c
amaanrahim <rahim.amaan@berkeley.edu>
2016-04-28 21:08:52 +0000

    Attachment

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do 
+ factory :attachment do
+   supporting_documentation_file_name { 'TestImage.jpeg' }
+   supporting_documentation_content_type { 'image/jpg' }
+   supporting_documentation_file_size { 1024 }
+ end
+ 
+  factory :attachment do
+    document { fixture_file_upload( File.join(Rails.root, 'spec', 'fixtures', 'TestImage.jpeg'), 'image/jpg') }
+  end
+end
