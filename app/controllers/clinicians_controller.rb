2ab9687320ad23c5d0362040fe0b90ad9d89520f
abraarakhter <akhterabraar@gmail.com>
2016-04-28 20:14:14 +0000

    can edit clinician profile

--- app/controllers/clinicians_controller.rb
+++ app/controllers/clinicians_controller.rb
@@ -33,6 +33,29 @@ class CliniciansController < ApplicationController
     
   def show
     @clinician = Clinician.find(params[:id])
+    @allowed = @clinician.id == current_clinician.id
+  end
+  
+  def edit
+    @clinician_id = params[:id]
+    @clinician = Clinician.find(@clinician_id)
+    if @clinician.id != current_clinician.id
+      flash[:warning] = "You cannot edit another clinician's profile"
+      redirect_to clinician_path(@clinician_id)
+    else
+      @clinician = Clinician.find(@clinician_id)
+    end
+  end
+  
+  def update
+    @clinician_id = params[:id].to_i
+    @clinician = Clinician.find(@clinician_id)
+    if @clinician.update(clinician_params)
+      flash[:success] = "Profile was succesfully updated"
+      redirect_to clinician_path(@clinician_id)
+    else
+      render 'edit'
+    end
   end
 
 end
