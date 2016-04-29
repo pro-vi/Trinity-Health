85a53d3faa2ed77047a89169599352c2c08132e3
abraarakhter <akhterabraar@gmail.com>
2016-04-29 05:54:08 +0000

    search query changes

--- app/models/case.rb
+++ app/models/case.rb
@@ -9,7 +9,7 @@ class Case < ActiveRecord::Base
     
     def self.search(search)
       if search
-        self.where("name LIKE ? OR diagnosis LIKE ? OR age = ? OR gender LIKE ?", search, search, search, search)
+        self.where("name LIKE ? OR diagnosis LIKE ? OR age = ? OR gender LIKE ?", search, search, search.to_i, search)
       end
     end
 end
